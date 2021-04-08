aec67e7c363e911a3635ef820fb9767cd2053295
Lukas Malina <lukas.malina@sapho.com>
2021-04-08 19:30:35 +0200

    Adding styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,24 @@
 import Link from "next/link";
 import Head from "next/head";
 import React from "react";
+import Layout from "../components/Layout";
 
-function Login(){
-  return(
-<div>
-  <Head>
-    <title>Login page</title>
-  </Head>
-  <body>
-  <input
-    placeholder="email@example.com"
-  />
-  <br/>
-  <input
-    placeholder="password"
-  />
-  <br/>
-  <button>Login</button>
-  <br/>
-  Take me to <Link href="/register_page"> register </Link> new lemur
-  </body>
-</div>);}
+function Login() {
+  return (
+    <Layout>
+      <Head>
+        <title>Test page</title>
+      </Head>
+      <body>
+        <input placeholder="email@example.com" />
+        <br />
+        <input placeholder="password" />
+        <br />
+        <button>Login</button>
+        <br />
+        Take me to <Link href="/register_page"> register </Link> new lemur
+      </body>
+    </Layout>
+  );
+}
 export default Login;
