a8f417663e8f820fa9e7ad51ef6cd8d193b57333
Jan Havlík <janh@Jans-MacBook-Pro.local>
2021-04-09 12:05:48 +0200

    backend login

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,17 +1,42 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { sampleUserData } from "../../utils/sample-data";
+import crypto from "crypto";
+import { Session, withIronSession } from "next-iron-session";
 
-const handler = (_req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (
+  req: NextApiRequest & { session: Session },
+  res: NextApiResponse
+) => {
   try {
-    if (!Array.isArray(sampleUserData)) {
-      throw new Error("Cannot find user data");
+    const body = JSON.parse(req.body);
+    const login = String(body.login);
+    const password = String(body.password);
+    const passhash = crypto
+      .createHash("md5")
+      .update("nasekouzelneslovo" + password)
+      .digest("hex");
+    console.log(passhash, login);
+    if (login === "vlk" && passhash === "b62b380e08439a53d02fc44aff759072") {
+      req.session.set("user", {
+        id: 230,
+        admin: true,
+      });
+      await req.session.save();
+      res.status(200).send("welcome");
+    } else {
+      res.status(401).send("go away from our village");
     }
-
-    res.status(200).json(sampleUserData);
   } catch (err) {
-    res.status(500).json({ statusCode: 500, message: err.message });
+    console.error("login error", err);
+    res.status(500).json({ statusCode: 500, message: "err.message" });
   }
-  console.log(_req.body);
 };
 
-export default handler;
+export default withIronSession(handler, {
+  password: "complex_password_at_least_32_characters_long",
+  cookieName: "lemur",
+  // if your localhost is served on http:// then disable the secure flag
+  cookieOptions: {
+    secure: process.env.NODE_ENV === "production",
+  },
+});
