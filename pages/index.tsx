36648b2c28f2a2a23cdb5c87ed6002d3929db5ec
Jan Mohyla <jan.mohyla@citrix.com>
2021-04-09 11:11:13 +0200

    login client side

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,39 @@
+import { useRef } from "react";
 import Layout from "../components/Layout";
 
-const IndexPage = () => (
-  <Layout title="Parking Lots">
-    <h1>Available spots</h1>
-    <ul>
-      <li>84.</li>
-      <li>85.</li>
-      <li>86.</li>
-      <li>87.</li>
-    </ul>
-  </Layout>
-);
+const IndexPage = () => {
+  const loginEl = useRef<HTMLInputElement>(null);
+  const passwordEl = useRef<HTMLInputElement>(null);
+  const sendData = () => {
+    fetch("/api/login", {
+      method: "POST",
+      body: JSON.stringify({
+        login: loginEl?.current?.value,
+        password: passwordEl?.current?.value,
+      }),
+    });
+    console.log("Odesilam data");
+  };
+  return (
+    <Layout title="Parking Lots">
+      <h1>Available spots</h1>
+      <ul>
+        <li>84.</li>
+        <li>85.</li>
+        <li>86.</li>
+        <li>87.</li>
+      </ul>
+      <label>
+        Login: <input ref={loginEl} />
+      </label>
+      <br />
+      <label>
+        Password: <input ref={passwordEl} />
+      </label>
+      <br />
+      <button onClick={sendData}>Login</button>
+    </Layout>
+  );
+};
 
 export default IndexPage;
