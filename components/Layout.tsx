9ee6eb66a682f6caa6d35843ca5198df498eea70
Lukas Malina <lukas.malina@sapho.com>
2021-04-22 19:10:18 +0200

    Final cleanup

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -18,22 +18,7 @@ const Layout = ({ children, title = "This is the default title" }: Props) => (
     <header>
       <nav>
         <div>
-          {/* <Link href="/home_page">
-            <a>Home</a>
-          </Link>{" "}
-          |{" "}
-          <Link href="/help">
-            <a>Help</a>
-          </Link>{" "}
-          |{" "}
-          <Link href="/about">
-            <a>About</a>
-          </Link>{" "}
-          |{" "}
-          <Link href="/users">
-            <a>Users List</a>
-          </Link>{" "} */}
-          {/* | <a href="/api/users">Users API</a>| <a href="/">Logout</a> */}
+          {/* To do: Clever navbar based on users location */}
           <Link href="/"> login </Link> |{" "}
           <Link href="/register_page"> register </Link>
         </div>
--- pages/help.tsx
+++
@@ -1,5 +0,0 @@
-import Layout from "../components/Layout";
-
-const IndexPage = () => <Layout title="Help">There is no help for now</Layout>;
-
-export default IndexPage;
